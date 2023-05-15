e964934a9083a2c7adbe76c3b590ff5db1c1fb2e
mikaelmonteirodev <mikaelmonteiro@gmail.com>
2023-05-15 10:11:40 -0300

    feat: use state

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,8 +1,26 @@
+import React from "react";
+
 export const Home = () => {
+
+    const [contador, setContador] = React.useState(0);    
+    const [darkMode, setDarkMode] = React.useState(false);
+
+    const decrementar = () => {
+        setContador(contador -1)
+    }
+
     return (
-        <>            
-            <h1>Home</h1>
+        <>    
+            <div className={darkMode ? 'centro dark' : 'centro'}>
+                <button onClick={() => setContador(contador + 1)}>Incrementar</button>
+                <h1>Contagem: ({contador})</h1>
+                <button onClick={decrementar}>Decrementar</button>                                
+            </div>        
+            <div onClick={() => setDarkMode(!darkMode)} className="switch">
+                {darkMode ? 'Acender' : 'Apagar'} Luz
+            </div>
         </>
     )
 }
+
 
--- src/components/pages/Sobre/index.tsx
+++ src/components/pages/Sobre/index.tsx
@@ -1,7 +1,20 @@
+import { Accordion } from "../../Accordion"
+
 export const Sobre = () => {
     return (
         <>
             <h1>Sobre</h1>
+            <ul className="sanfonas">
+                <li>
+                    <Accordion />
+                </li>
+                <li>
+                    <Accordion />
+                </li>
+                <li>
+                    <Accordion />
+                </li>
+            </ul>
         </>
     )
 }
